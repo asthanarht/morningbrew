33df8bd66d436c7187caed14fc64f004a74c2055
ROHIT <asthanarht@gmail.com>
2018-01-17 00:34:31 -0800

    * MorningBrew.userprefs: add fab and UI improve
    * MorningBrew.csproj:
    * BrewsHome.xaml:
    * ImageButton.cs:
    * MorningBrew.pdb:
    * MorningBrew.dll:
    * IconFAB.png:
    * MorningBrew.dll:
    * MorningBrew.pdb:
    * MorningBrew.iOS.csproj:
    * CustomWebViewPage.xaml:
    * bookmarkfab.png:
    * MorningBrew.pdb:
    * MorningBrew.dll:
    * MorningBrewiOS.exe:
    * MorningBrewiOS.pdb:
    * MorningBrewiOS.exe:
    * MorningBrewiOS.pdb:
    * MorningBrew.csproj.CoreCompileInputs.cache:
    * MorningBrew.iOS.csproj.FileListAbsolute.txt:
    * MorningBrew.iOS.csprojResolveAssemblyReference.cache:
    * MorningBrew.dll:
    * MorningBrew.pdb:
    * MorningBrew.dll:
    * MorningBrew.pdb:
    * MorningBrew.dll:
    * MorningBrew.pdb:
    * MorningBrewiOS.pdb:
    * MorningBrewiOS.exe:
    * MorningBrew.pdb:
    * MorningBrewiOS.pdb:
    * MorningBrewiOS.exe:
    * MorningBrewiOS.pdb:
    * MorningBrewiOS.exe:
    * MorningBrew.dll:
    * MorningBrewiOS.exe:
    * MorningBrewiOS.pdb:

---
+++ MorningBrew/MorningBrew/Control/ImageButton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace MorningBrew
+{
+
+    public class ImageButton : Image
+    {
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create("Command", typeof(ICommand), typeof(ImageButton), null);
+
+        public static readonly BindableProperty CommandParameterProperty =
+            BindableProperty.Create("CommandParameter", typeof(object), typeof(ImageButton), null);
+
+        public event EventHandler ItemTapped = (e, a) => { };
+
+        public ImageButton()
+        {
+            Initialize();
+        }
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        private ICommand TransitionCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    AnchorX = 0.48;
+                    AnchorY = 0.48;
+                    await this.ScaleTo(0.8, 50, Easing.Linear);
+                    await Task.Delay(100);
+                    await this.ScaleTo(1, 50, Easing.Linear);
+                    Command?.Execute(CommandParameter);
+
+                    ItemTapped(this, EventArgs.Empty);
+                });
+            }
+        }
+
+        public void Initialize()
+        {
+            GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = TransitionCommand
+            });
+        }
+    }
+}
