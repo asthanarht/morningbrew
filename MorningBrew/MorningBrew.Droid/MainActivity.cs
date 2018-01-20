91b96d4103f0d6fe62c10a2cefdb34b9c9e6c327
ROHIT <asthanarht@gmail.com>
2018-01-20 02:32:29 -0800

    * MorningBrew.dll: update android theme
    * MainActivity.cs:
    * build.props:
    * MorningBrew.Droid.csproj:
    * MorningBrew.pdb:
    * styles.xml:
    * Resource.Designer.cs:
    * styles.xml:
    * MorningBrew.Droid.pdb:
    * MorningBrew.Droid.dll:
    * MorningBrew.Droid.dll:
    * MorningBrew.Droid.pdb:
    * classes.dex:
    * MorningBrew.pdb:
    * MorningBrew.dll:
    * splash.png:
    * brewicon.png:
    * AndroidManifest.xml:
    * MorningBrew.Droid.pdb:
    * MorningBrew.Droid.dll:
    * MorningBrew.Droid.pdb:
    * MorningBrew.Droid.dll:
    * packaged_resources:
    * MorningBrew.dll:
    * MorningBrew.pdb:
    * classes.zip:
    * R.java:
    * MorningBrew.Droid.pdb:
    * AndroidManifest.xml:
    * MorningBrew.Droid.dll:
    * R.java:
    * MorningBrew.Droid.csproj.FileListAbsolute.txt:
    * MorningBrew.Droid.csproj.CoreCompileInputs.cache:
    * R$id.class:
    * R$style.class:
    * R$string.class:
    * R$layout.class:
    * R$integer.class:
    * MorningBrew.Droid.csprojResolveAssemblyReference.cache:
    * R$drawable.class:
    * R$styleable.class:
    * R$interpolator.class:

--- MorningBrew/MorningBrew.Droid/MainActivity.cs
+++ MorningBrew/MorningBrew.Droid/MainActivity.cs
@@ -10,11 +10,15 @@ using Android.OS;
 
 namespace MorningBrew.Droid
 {
-    [Activity(Label = "MorningBrew", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MorningBrew", Theme = "@style/splashscreen", Icon = "@drawable/brewicon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            // Name of the MainActivity theme you had there before.
+            // Or you can use global::Android.Resource.Style.ThemeHoloLight
+            base.SetTheme(global::Android.Resource.Style.ThemeHoloLight);
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
